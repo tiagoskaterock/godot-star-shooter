9b20a7ce0039492845e392292979f1ea79fffd82
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-12-13 07:35:45 -0300

    53. Creating the Player Scene

--- README.md
+++ README.md
@@ -14,5 +14,5 @@
 </p>
 
 <p align="center">
-    <!-- <img src="1.gif" alt="Pong Clone" /> -->
+    <img src="1.png" alt="Star Shooter" />
 </p>
---
+++ star-shooter/scripts/Player.gd
@@ -0,0 +1,55 @@
+extends KinematicBody2D
+
+export (int) var max_speed = 500
+var acceleration = 900
+var deacceleration = 500
+var input = Vector2.ZERO
+
+func _physics_process(delta):
+	check_move(delta)
+	check_shoot()
+	move_and_slide(input)
+	
+func check_move(delta):
+	check_horizontal_move(delta)	
+	check_vertical_move(delta)
+	
+func check_horizontal_move(delta):		
+	if Input.is_action_pressed("move_left") and input.x > -max_speed:
+		go_left(delta)
+	if Input.is_action_pressed("move_right") and input.x < max_speed:
+		go_right(delta)
+	if Input.is_action_pressed("move_left") and Input.is_action_pressed("move_right"):
+		deaccelerate_horizontal(delta)
+	if ! Input.is_action_pressed("move_left") and ! Input.is_action_pressed("move_right"):
+		deaccelerate_horizontal(delta)
+
+func check_vertical_move(delta):
+	if Input.is_action_pressed("move_up") and input.y > -max_speed:
+		go_up(delta)
+	if Input.is_action_pressed("move_down") and input.y < max_speed:
+		go_down(delta)
+	if Input.is_action_pressed("move_down") and Input.is_action_pressed("move_up"):
+		deaccelerate_vertical(delta)
+	if ! Input.is_action_pressed("move_down") and ! Input.is_action_pressed("move_up"):
+		deaccelerate_vertical(delta)
+				
+func go_left(delta): input.x -= acceleration * delta
+
+func go_right(delta): input.x += acceleration * delta
+
+func go_up(delta): input.y -= acceleration * delta
+	
+func go_down(delta): input.y += acceleration * delta
+		
+func deaccelerate_horizontal(delta):
+	if input.x < 0: input.x += deacceleration * delta
+	if input.x > 0: input.x -= deacceleration * delta
+	
+func deaccelerate_vertical(delta):
+	if input.y < 0: input.y += deacceleration * delta
+	if input.y > 0: input.y -= deacceleration * delta
+
+func check_shoot():
+	if Input.is_action_just_pressed("shoot"):
+		print('shoot')
