b1359c1abf843908d6faebeaacfdce02ad0401ce
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-12-14 01:04:48 -0300

    57. Taking Damage

--- star-shooter/scripts/Enemy.gd
+++ star-shooter/scripts/Enemy.gd
@@ -1,7 +1,16 @@
 extends Area2D
 
 export (int) var speed = 150
+export (int) var health = 3
+const type = 'enemy'
 
 func _physics_process(delta):
 	global_position.y += speed * delta
 
+func enemy_hit():
+	health -= 1
+	print('enemy health: ' + str(health))
+	if health < 1: enemy_dies()
+	
+func enemy_dies():
+	queue_free()
--- star-shooter/scripts/Player.gd
+++ star-shooter/scripts/Player.gd
@@ -1,22 +1,36 @@
 extends KinematicBody2D
 
 export (int) var max_speed = 500
-var acceleration = 2500
-var deacceleration = acceleration
+var acceleration = 2000
+var deacceleration = acceleration / 2
 var input = Vector2.ZERO
+var min_speed_to_move = 15
+var intitial_position = Vector2(256, 872)
+var lives = 5
+var poinst = 0
 
-func _physics_process(delta):	
+func _physics_process(delta):
+	check_min_speed()
 	check_move(delta)
 	check_shoot()
-	global_position.x = clamp(global_position.x, 50, 490)
-	global_position.y = clamp(global_position.y, 40, 920)
+	limit_on_screen()
 	move_and_slide(input)
 	
+func limit_on_screen():
+	global_position.x = clamp(global_position.x, 50, 490)
+	global_position.y = clamp(global_position.y, 40, 920)
+	
+func check_min_speed():
+	if input.y < min_speed_to_move and input.y > -min_speed_to_move: 
+		input.y = 0
+	if input.x < min_speed_to_move and input.x > -min_speed_to_move: 
+		input.x = 0
+	
 func check_move(delta):
-	check_horizontal_move(delta)	
+	check_horizontal_move(delta)
 	check_vertical_move(delta)
 	
-func check_horizontal_move(delta):		
+func check_horizontal_move(delta):
 	if Input.is_action_pressed("move_left") and input.x > -max_speed:
 		go_left(delta)
 	if Input.is_action_pressed("move_right") and input.x < max_speed:
@@ -35,6 +49,8 @@ func check_vertical_move(delta):
 		deaccelerate_vertical(delta)
 	if ! Input.is_action_pressed("move_down") and ! Input.is_action_pressed("move_up"):
 		deaccelerate_vertical(delta)
+	if input.y > max_speed: input.y = max_speed
+	if input.y < -max_speed: input.y = -max_speed	
 				
 func go_left(delta): input.x -= acceleration * delta
 
@@ -46,7 +62,7 @@ func go_down(delta): input.y += acceleration * delta
 		
 func deaccelerate_horizontal(delta):
 	if input.x < 0: input.x += deacceleration * delta
-	if input.x > 0: input.x -= deacceleration * delta
+	if input.x > 0: input.x -= deacceleration * delta	
 	
 func deaccelerate_vertical(delta):
 	if input.y < 0: input.y += deacceleration * delta
@@ -55,3 +71,25 @@ func deaccelerate_vertical(delta):
 func check_shoot():
 	if Input.is_action_just_pressed("shoot"):
 		print('shoot')
+
+func _on_Area2D_area_entered(area):
+	if area.type == 'enemy':
+		area.enemy_hit()
+		player_die()	
+	
+func player_die():
+	$Explosion.play()
+	lose_life()
+	reset_position()
+	reset_speed()
+	
+func reset_position():
+	position = intitial_position
+	
+func reset_speed():
+	input.x = 0
+	input.y = 0
+	
+func lose_life():
+	lives -= 1
+	print("Player Lives: " + str(lives))
