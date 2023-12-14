3a98bb91eac9a2d79c97b71d68bee3e07142eac7
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-12-14 03:45:22 -0300

    57. Taking Damage improved

--- star-shooter/scripts/Enemy.gd
+++ star-shooter/scripts/Enemy.gd
@@ -8,9 +8,17 @@ func _physics_process(delta):
 	global_position.y += speed * delta
 
 func enemy_hit():
+	$HitFX.play()
 	health -= 1
 	print('enemy health: ' + str(health))
-	if health < 1: enemy_dies()
+	if health < 1: 
+		print('morre porra')
+		enemy_dies()
 	
 func enemy_dies():
+	$DeadFX.play()
+	$Sprite.visible = false
+	$TimerToDie.start()
+
+func _on_TimerToDie_timeout():
 	queue_free()
---
+++ star-shooter/scripts/Laser.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+var speed = 300
+var vertical_direction
+export var type = 'player_laser'
+export (int) var damage = 1
+
+func _ready():
+	if type == 'player_laser': vertical_direction = -1
+	elif type == 'enemy_laser': vertical_direction = 1
+
+func _physics_process(delta):
+	global_position.y += speed * vertical_direction * delta
+	
+func _on_Laser_area_entered(area):	
+	if type == 'player_laser' and area.type == 'enemy': 
+		area.enemy_hit()
+		queue_free()
+	if type == 'enemy_laser' and area.type == "player_laser":
+		laser_hit()
+	if type == 'player_laser' and area.type == 'enemy_laser':
+		laser_hit()
+		
+func laser_hit():
+	speed = 0
+	$Sprite.visible = false
+	$DeadFX.play()
+	$TimerToDie.start()		
+
+func _on_TimerToDie_timeout():
+	queue_free()
